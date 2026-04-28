afee7eac84757661891ad61acde6ed37f468eacd
Caio Marinho <rm572873@fiap.com.br>
2026-04-28 10:21:25 -0300

    criacao das variaveis e inputs do usuario

---
+++ derivada.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    float a, b, c;
+
+    printf("==== DERIVADA DE FUNCAO SIMPLES ====\n\n");
+    printf("Modelo de funcao: f(x) = ax^2 + bx + c");
+    printf("Indique os coeficientes a, b e c da funcao: \n");
+
+    printf("Coeficiente a: ");
+    scanf("%f", &a);
+    printf("Coeficiente b: ");
+    scanf("%f", &b);
+    printf("Coeficiente c: ");
+    scanf("%f", &c);
+
+    return 0;
+}
